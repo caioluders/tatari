954f661f59a64c20d8797866a67709b1f265de03
Déborah Mesquita <deh.mesquita@hotmail.com>
2015-11-13 16:37:38 -0300

    Adding first screen before tabController

---
+++ Tatari/BorderFeedCellView.swift
@@ -0,0 +1,23 @@
+//
+//  BorderFeedCellView.swift
+//  Tatari
+//
+//  Created by Déborah Mesquita on 13/11/15.
+//  Copyright © 2015 Caio Araújo. All rights reserved.
+//
+
+import UIKit
+
+class BorderFeedCellView: UIView {
+
+    override func drawRect(rect: CGRect){
+        let context = UIGraphicsGetCurrentContext()
+        CGContextSetLineWidth(context, 4.0)
+        CGContextSetStrokeColorWithColor(context,
+            UIColor.blueColor().CGColor)
+        let rectangle = CGRectMake(60,170,200,80)
+        CGContextAddRect(context, rectangle)
+        CGContextStrokePath(context)
+    }
+
+}
