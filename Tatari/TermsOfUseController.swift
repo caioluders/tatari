5d7e476a07ff001af6606466ea21bc875d58aa6a
LadyDay <day_13k@yahoo.com>
2016-02-29 19:17:32 -0300

    EULA feito.

---
+++ Tatari/TermsOfUseController.swift
@@ -0,0 +1,21 @@
+//
+//  TermsOfUseController.swift
+//  Tatari
+//
+//  Created by Dayane Kelly Rodrigues da Silva on 2/29/16.
+//  Copyright © 2016 Caio Araújo. All rights reserved.
+//
+
+import UIKit
+
+class TermsOfUseController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+    }
+}
